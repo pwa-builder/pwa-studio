bc9db67f4e9173efefb8c7a69c7a51c8053df9dd
Zach Teutsch <88554871+zateutsch@users.noreply.github.com>
2021-12-07 16:38:08 -0800

    Fixed input prompt string and formatting

--- src/services/StarterService.ts
+++ src/services/StarterService.ts
@@ -1,29 +1,25 @@
 import * as vscode from 'vscode';
 const shell = require("shelljs");
 
-const repositoryInputPrompt: string = "Test prompt test prompt";
+const repositoryInputPrompt: string = "Enter the name you would like to use for your PWA's repository.";
 const repositoryInputPlaceholder: string = "Enter your repository name here.";
-
 const noNameSelectedWarning: string = "No repository name provided. New PWA Starter process cancelled.";
 const noGitWarning: string = "This command requires git. Install git at https://git-scm.com/";
 const noNpmWarning: string = "This command requires npm. Install git at https://www.npmjs.com/";
-
 const starterRepositoryURI: string = "https://github.com/pwa-builder/pwa-starter.git";
 
 var repositoryName: string | undefined = undefined;
-
 const vsTerminal = vscode.window.createTerminal();
 
 export async function setUpLocalPwaStarterRepository(): Promise<void> 
 {
     await getRepositoryNameFromInputBox();
-
+    
     if(repositoryName !== undefined)
     {
         initStarterRepository();
         openRepositoryWithCode();
-    }
-    
+    }    
 }
 
 async function getRepositoryNameFromInputBox(): Promise<void> 
