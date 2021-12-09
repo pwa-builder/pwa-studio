33f1c3ea1183a9ad4e6ffd377b99731bee67d5f0
Zach Teutsch <88554871+zateutsch@users.noreply.github.com>
2021-12-09 10:59:21 -0800

    StarterService renamed to new-pwa-starter

--- src/extension.ts
+++ src/extension.ts
@@ -1,7 +1,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
-import { setUpLocalPwaStarterRepository } from './services/StarterService';
+import { setUpLocalPwaStarterRepository } from './services/new-pwa-starter';
 import { handleServiceWorkerCommand } from './services/service-worker';
 
 // this method is called when your extension is activated
