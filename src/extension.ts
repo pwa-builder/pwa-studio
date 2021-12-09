21243544d7e57d5f4a060736f00346937253e22d
Justin Willis <jgw9617@gmail.com>
2021-12-09 12:15:54 -0800

    chore(): clean up

--- src/extension.ts
+++ src/extension.ts
@@ -7,17 +7,7 @@ import { handleServiceWorkerCommand } from "./services/service-worker";
 const serviceWorkerCommandId = "pwa-studio.serviceWorker";
 const newPWAStarterCommandId = "pwa-studio.newPwaStarter";
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-  // Use the console to output diagnostic information (console.log) and errors (console.error)
-  // This line of code will only be executed once when your extension is activated
-  console.log('Congratulations, your extension "pwa-studio" is now active!');
-
-  // The command has been defined in the package.json file
-  // Now provide the implementation of the command with registerCommand
-  // The commandId parameter must match the command field in package.json
-
   const myStatusBarItem = vscode.window.createStatusBarItem(
     vscode.StatusBarAlignment.Right,
     100
@@ -45,5 +35,4 @@ export function activate(context: vscode.ExtensionContext) {
   context.subscriptions.push(myStatusBarItem);
 }
 
-// this method is called when your extension is deactivated
 export function deactivate() {}
