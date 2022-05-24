6705d00e206ae197d1fa301b52a2a063b9f03e3d
Justin Willis (HE / HIM) <juwillis@microsoft.com>
2022-05-24 10:56:53 -0700

    fix(): correct insert token + remove throw

--- src/services/usage-analytics.ts
+++ src/services/usage-analytics.ts
@@ -5,7 +5,7 @@ export function initAnalytics() {
   try {
     // check flag first
     if (getFlag("analytics") === true) {
-      setup().start()
+      setup("#{ANALYTICS_CODE}#")
       .setAutoDependencyCorrelation(false)
       .setAutoCollectRequests(false)
       .setAutoCollectPerformance(false, false)
@@ -19,7 +19,6 @@ export function initAnalytics() {
   }
   catch (err) {
     console.error("Error initializing analytics", err);
-    throw new Error(`Error initializing analytics: ${err}`);
   }
 }
 
